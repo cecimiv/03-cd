6418ca6cc781a0f6c6b98a5bb6808fa39b91a3c4
Cecilie Marie Florin Iversen <ceiv001@EGMS.no>
2021-09-14 11:41:49 +0200

    test fix test

--- payload.test.js
+++ payload.test.js
@@ -1,7 +1,7 @@
   const payload = require("./payload");
 
   test("Creates at least age correctly in payload", () => {
-      expect(payload("glenn", "Bech", 44, "DevOps").age).toBe(4);
+      expect(payload("glenn", "Bech", 44, "DevOps").age).toBe(44);
   });
 
   test("Creates at least age correctly in payload", () => {
